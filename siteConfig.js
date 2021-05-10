be169dae972a19f5a0dd53ba40bb13f94e95e0c2
Denis Kuznetsov <kuznecov@MacBook-Pro-2.local>
2021-05-10 19:56:51 +0300

    fix desc

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: "London Night and Day",
+  name: "Начни с понедельника",
   shortName: "",
-  description: " ",
+  description:
+    "Принятие решений, привычки, изменение собственных майндсетов, психология, продуктовый менеджмент, предпринимательство, разные инсайты, работающие системы и что-нибудь наверняка еще",
   url: "https://www.jooplaan.com", // Domain of your site without prefix!
   prefix: "/",
   author: "jooplaan",
-  twitter: ""
+  twitter: "onmondays"
 };
