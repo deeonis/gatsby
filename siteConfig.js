1ee3a65a24d1ff1df431abb3e8c195b809c11d3c
Denis Kuznetsov <kuznecov@MacBook-Pro-2.local>
2021-05-10 22:52:07 +0300

    fix title

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "DEZO DESIGN",
   shortName: "",
-  description: "О том, как становиться лучше",
+  description: "Interior design",
   url: "https://www.jooplaan.com", // Domain of your site without prefix!
   prefix: "/",
   author: "jooplaan",
